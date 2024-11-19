e38a98923a98972c13439ab69943f4849df3909d
Shresta-Voruganti <22bd1a053z@gmail.com>
2024-11-19 14:03:06 +0530

    uploaded files

---
+++ src/main/java/com/app/demo/javaMavExample/App.java
@@ -0,0 +1,13 @@
+package com.app.demo.javaMavExample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Shresta Voruganti" );
+    }
+}
